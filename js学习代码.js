85dff1a6e35d6c5309338c99e3214c3428a087c2
公主的觅风 <19881962@qq.com>
2017-10-27 06:01:39 +0800

    Update js学习代码.js

--- js学习代码.js
+++ js学习代码.js
@@ -24,3 +24,22 @@ for(var i = 0; i<1000000; i++)
     total = total + i.toString();
     history.pushState(0,0,total);
 }
+//滚动监听
+$(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(window).scrollTop();//网页被卷起来的高度
+        var winheight = $(window).height();//页面可视区域的高度
+        $("div").each(function(){
+            //如果元素距离页面最顶部小于 网页目前的高度的时候
+            if(this.offsetTop < scrolltop + winheight - 100){
+                $(this).children().addClass("bounceInUp");
+            }
+            if(this.offsetTop < scrolltop + winheight - 800){
+                $(this).children().removeClass("bounceInUp");
+            }
+            if(this.offsetTop > scrolltop + winheight + 100){
+                $(this).children().removeClass("bounceInUp");
+            }
+        });
+    });
+});
